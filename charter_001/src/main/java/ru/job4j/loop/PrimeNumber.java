5aae0c11393ff7268986bca2a9cde5c07a7ae063
kiaromono <kiaromono@gmail.com>
2020-03-04 14:05:15 +0300

    5.6. Простые числа.

--- charter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ charter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,6 +2,7 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
 
+
     public int calc(int finish) {
         int count = 1;
         for (int number = 2; number <= finish; number++) {
--- charter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ charter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+
 public class PrimeNumberTest {
     @Test
     public void when5() {
