4607da4d9dcd68c24f1b33cffc2d7a152132fd1b
kiaromono <58180719+kiaromono@users.noreply.github.com>
2020-04-01 14:00:38 +0300

    5.6. Простые числа. Добавила условие для единицы

--- charter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ charter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = (number != 1);
         for (int i = number - 1; i > 1; i--) {
-            if (number % i == 0) {
+            if ((number % i) == 0) {
                 prime = false;
                 break;
             }
-
         }
         return prime;
     }
